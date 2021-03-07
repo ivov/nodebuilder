5c1f0447c71bda044042ce41bfa68533130b30f0
Iván Ovejero <ivov.src@gmail.com>
2021-03-07 19:47:37 -0300

    Fix clear command

--- src/scripts/clear.ts
+++ src/scripts/clear.ts
@@ -1,18 +1,24 @@
-import fs, { existsSync } from "fs";
+import fs from "fs";
 import { join } from "path";
 import { promisify } from "util";
 
 const deleteDir = promisify(fs.rmdir);
 const deleteFile = promisify(fs.unlink);
 
-const descriptionsDir = join("src", "output", "descriptions");
+const outputDir = join(__dirname, "..", "..", "src", "output");
+const descriptionsDir = join(outputDir, "descriptions");
 
-if (!existsSync(descriptionsDir)) {
-  deleteDir(descriptionsDir);
-}
+(async () => {
+  if (fs.existsSync(descriptionsDir)) {
+    await deleteDir(descriptionsDir, { recursive: true });
+  }
+})();
 
 const filesToDelete = fs
-  .readdirSync(join("src", "output"))
+  .readdirSync(outputDir)
+  .filter((file) => fs.lstatSync(join(outputDir, file)).isFile())
   .filter((file) => file !== ".gitkeep");
 
-filesToDelete.forEach(deleteFile);
+filesToDelete.forEach(async (file) => {
+  await deleteFile(join(outputDir, file));
+});
