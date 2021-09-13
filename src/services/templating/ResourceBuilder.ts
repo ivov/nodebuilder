add151fbd365c94d51df10d53b456bccfa4425ab
Iván Ovejero <ivov.src@gmail.com>
2021-09-13 10:54:34 +0200

    Update limit description

--- src/services/templating/ResourceBuilder.ts
+++ src/services/templating/ResourceBuilder.ts
@@ -68,7 +68,7 @@ export default class ResourceBuilder {
       name: 'limit',
       type: 'number',
       default: 50,
-      description: 'How many results to return',
+      description: 'Max number of results to return',
       typeOptions: {
         minValue: 1,
       },
